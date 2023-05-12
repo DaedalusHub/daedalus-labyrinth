ffcdda1cc129efdd5845789fb65875ba47737d3b
Sam Biggins <sambiggins@gmail.com>
2023-05-11 23:23:08 -0700

    test: update input component test

--- src/user_interface/indexInputForm.cy.tsx
+++ src/user_interface/indexInputForm.cy.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import InputForm from './index'
+import React from 'react';
+import InputForm from './index';
+import { SearchEngine } from '../data_retrieval/search_engine';
+import { Source } from '@src/data_retrieval/source';
+
+class MockSearchEngine extends SearchEngine {
+  async search(query: string): Promise<Source[]> {
+    return [];
+  }
+}
 
 describe('<InputForm />', () => {
   it('renders', () => {
+    // Create a new instance of the mock search engine
+    const searchEngine = new MockSearchEngine('key', 'cx');
+
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<InputForm />)
-  })
-})
+    cy.mount(<InputForm searchEngine={searchEngine} />);
+  });
+});
